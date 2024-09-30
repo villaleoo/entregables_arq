32c588f524ca1ef4eb56fcafb1328856186c2b23
Facundo Olesen <faolesen@alumnos.exa.unicen.edu.ar>
2024-09-30 14:24:46 -0300

    Delete helpers

--- Integrador2/src/main/java/Integrador2/Main.java
+++ Integrador2/src/main/java/Integrador2/Main.java
@@ -1,14 +1,20 @@
 package Integrador2;
 
+import Integrador2.Entities.Carrera;
 import Integrador2.Entities.Estudiante;
 import Integrador2.Repository.CarreraRepository;
 import Integrador2.Repository.InscripcionRepository;
 import Integrador2.Repository.EstudianteRepository;
-import Integrador2.Utils.HelperInitDB;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
 import java.time.LocalDate;
 
 
@@ -16,8 +22,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("MySQL");
         EntityManager em = emf.createEntityManager();
-        HelperInitDB helper = new HelperInitDB();
-        helper.insertDefaultData(em);
+        insertDefaultData(em);
 
 
         EstudianteRepository estudianteRepository = EstudianteRepository.getInstance(em);
@@ -125,5 +130,61 @@ public class Main {
 
         em.getTransaction().commit();
         em.close();
+    }
+
+    private static Iterable<CSVRecord> getData(String archivo) throws IOException {
+        String path = "src\\main\\resources\\" + archivo;
+        Reader in = new FileReader(path);
+        String[] header = {};
+        CSVParser csvParser = CSVFormat.EXCEL.withHeader(header).parse(in);
+
+        Iterable<CSVRecord> records = csvParser.getRecords();
+        return records;
+    }
+
+    public static void insertDefaultData(EntityManager em) throws Exception {
+        try {
+            for (CSVRecord row : getData("Carreras.csv")) {
+                if (row.size() >= 1) {
+                    String nombreCarrera = row.get(0);
+
+                    if (!nombreCarrera.isEmpty()) {
+                        try {
+                            Carrera carrera = new Carrera(nombreCarrera);
+                            CarreraRepository.getInstance(em).persist(carrera);
+                        } catch (NumberFormatException e) {
+                            System.err.println("Error de formato en datos de carrera: " + e.getMessage());
+                        }
+                    }
+                }
+            }
+            for (CSVRecord row : getData("Estudiantes.csv")) {
+                if (row.size() >= 7) {
+                    String nombre = row.get(0);
+                    String apellido = row.get(1);
+                    String edad = row.get(2);
+                    String genero = row.get(3);
+                    String documento = row.get(4);
+                    String ciudad = row.get(5);
+                    String nroLibreta = row.get(6);
+
+                    if (!nombre.isEmpty() && !apellido.isEmpty()
+                            && !edad.isEmpty() && !genero.isEmpty() && !documento.isEmpty()
+                            && !ciudad.isEmpty() && !nroLibreta.isEmpty()) {
+                        try {
+                            int age = Integer.parseInt(edad);
+                            int dni = Integer.parseInt(documento);
+                            int libreta = Integer.parseInt(nroLibreta);
+                            Estudiante estudiante = new Estudiante(nombre, apellido, age, genero, dni, ciudad, libreta);
+                            EstudianteRepository.getInstance(em).persist(estudiante);
+                        } catch (NumberFormatException e) {
+                            System.err.println("Error de formato en datos de estudiante: " + e.getMessage());
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
