f4df29d6153f73651f0da0e9ae1c55b8f68aac7d
leopoldo <villaleoo@hotmail.com>
2024-09-29 23:20:32 -0300

    agregado modulos y directorios correctamente

--- integrador1/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example.integrador1 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.example.integrador1 to javafx.fxml;
-    exports org.example.integrador1;
-}
--- integrador2/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example.integrador2 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.example.integrador2 to javafx.fxml;
-    exports org.example.integrador2;
-}
